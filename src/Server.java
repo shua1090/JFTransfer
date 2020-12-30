a30c125fb910ccb6c93444dbe1706801d731eaaf
Shynn Lawrence <61219106+shua1090@users.noreply.github.com>
2020-12-30 09:44:22 -0800

    Demonstration of File Chooser

--- src/Server.java
+++ src/Server.java
@@ -1,12 +1,30 @@
 import javax.swing.*;
-
+import java.awt.*;
 class GUI{
+    JLabel selectedFile;
+    
+    GUI(){
+
+    }
+
     public static void main(String[] args){
         JFrame frame = new JFrame();
         frame.setLayout(null);
-        frame.add(new JLabel("HOOHOH"));
+        
+        JPanel panel = new JPanel();
+        var gbl = new GridBagLayout();
+        panel.setLayout(gbl);
+        JFileChooser fc = new JFileChooser("Pick a file");
+        panel.add(fc);
+        frame.add (panel);
+        int returnVal = fc.showDialog(panel, "Attach");
+
+        System.out.println(returnVal);
+        System.out.println(fc.getSelectedFile());
+
         frame.setVisible(true);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+
     }
 }
 
