cd6f719047ca454ccc80d768bbda16d4714dbf7c
Shynn Lawrence <61219106+shua1090@users.noreply.github.com>
2020-12-29 23:16:17 -0800

    Update GUI

---
+++ src/Server.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+class GUI{
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setLayout(null);
+        frame.add(new JLabel("HOOHOH"));
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+    }
+}
+
+
+
+// public class Server{
+// }
